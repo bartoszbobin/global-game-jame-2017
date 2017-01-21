54035ae70ed00244e23a85da94d6bc2c84d925ce
Michal Michalczuk <michalczukm@gmail.com>
2017-01-21 15:05:03 +0100

    added rock-obstacles group

--- src/app/sprites/obstacle.ts
+++ src/app/sprites/obstacle.ts
@@ -1,5 +1,6 @@
 import {ObstacleAsset} from '../data/models';
 import * as Phaser from 'phaser';
+import {getRandomInt} from '../utils';
 
 export class Obstacle extends Phaser.Sprite {
     private get doesDamage(): number { return 10; }
@@ -21,7 +22,7 @@ export class Obstacle extends Phaser.Sprite {
         this.p2Body.angle = this.rotationInArcs;
 
         // by convention
-        this.p2Body.loadPolygon(`${this.asset}Physics`, this.asset)
+        this.p2Body.loadPolygon(`${this.asset}Physics`, this.asset);
     }
 }
 
@@ -34,6 +35,16 @@ export class StickObstacle extends Obstacle {
     }
 }
 
+export class WoodObstacle extends Obstacle {
+    constructor(game: Phaser.Game, positionX: number, positionY: number, rotationInArcs: number = 0) {
+        super(game, 'stick', positionX, positionY, rotationInArcs);
+
+        this.scale = new Phaser.Point(2, 2);
+        this.p2Body.mass = 450;
+        this.p2Body.static = false;
+    }
+}
+
 export class RockObstacle extends Obstacle {
     constructor(game: Phaser.Game, positionX: number, positionY: number, rotationInArcs: number = 0) {
         super(game, 'rock-obstacle', positionX, positionY, rotationInArcs);
@@ -41,13 +52,30 @@ export class RockObstacle extends Obstacle {
 }
 
 export class RocksGroupObstacle extends Phaser.Sprite {
+    protected get p2Body():Phaser.Physics.P2.Body { return this.body };
+
     constructor(game: Phaser.Game, positionX: number, positionY: number, rotationInArcs: number = 0, rocksNumber: number = 1) {
-        super(game, positionX, positionY, 'rock-group');
-
-        for(let i; i<rocksNumber; i++) {
-            this.addChild(new RockObstacle(game, 0, 0));
-        }
-
+        super(game, positionX, positionY);
         this.angle = rotationInArcs;
+
+        this.loadChildRocks(rocksNumber);
+        this.game.physics.p2.enable(this);
+        this.p2Body.static = true;
+    }
+
+    private loadChildRocks(rocksNumber: number) {
+        const rotationStep = 360 / rocksNumber;
+        const radius = 50;
+
+
+        for(let i = 0; i < rocksNumber; i++) {
+            const angle = getRandomInt(i * rotationStep, (i + 1) * rotationStep) * (Math.PI / 180);
+            const x = Math.cos(angle) * radius;
+            const y = Math.sin(angle) * radius;
+
+            let rock = new RockObstacle(this.game, x, y, angle);
+
+            this.addChild(rock);
+        }
     }
 }
