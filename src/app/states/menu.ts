3dbd321836a6b1f9f1c9dc2e74cc1bf7959f3f40
Karolus <karol.gierszewski@gmail.com>
2017-01-21 16:20:40 +0100

    Menu uppercase

--- src/app/states/menu.ts
+++ src/app/states/menu.ts
@@ -15,7 +15,7 @@ export class MenuState extends Phaser.State {
         this.load.image('menu-start-button', './assets/images/menu-start-button.png');
         this.load.image('menu-background', './assets/images/menu-background.png');
 
-        let letterFont = '64px Chewy';
+        let letterFont = '55px Chewy';
         let letterStyle = '#000000';
 
         this.letter1 = this.game.make.bitmapData(100, 100);
@@ -30,13 +30,13 @@ export class MenuState extends Phaser.State {
         this.letter3.context.font = letterFont;
         this.letter3.context.fillStyle = letterStyle;
 
-        this.input.keyboard.addCallbacks(this, null, null, this.addLetter);
+        this.input.keyboard.addCallbacks(this, null, null, this.addLetter);        
     }
 
     create() {
         this.add.tileSprite(0, 0, 1280, 620, 'menu-background');
         this.startButton = this.add.button(425, 425, 'menu-start-button', this.startGame, this);
-        this.startButton.inputEnabled = false;
+        this.startButton.inputEnabled = false;   
     }
 
     startGame() {
@@ -46,15 +46,15 @@ export class MenuState extends Phaser.State {
 
     addLetter(char) {
         if (this.userName.length === 0) {
-            this.letter1.context.fillText(char, 55, 55);
+            this.letter1.context.fillText(char.toUpperCase(), 55, 55);
             this.letter1.addToWorld(500, 300);
             this.userName += char;
         } else if (this.userName.length === 1) {
-            this.letter2.context.fillText(char, 55, 55);
+            this.letter2.context.fillText(char.toUpperCase(), 55, 55);
             this.letter2.addToWorld(570, 300);
             this.userName += char;
         } else if (this.userName.length === 2) {
-            this.letter3.context.fillText(char, 55, 55);
+            this.letter3.context.fillText(char.toUpperCase(), 55, 55);
             this.letter3.addToWorld(640, 300);
             this.userName += char;
             this.startButton.inputEnabled = true;
